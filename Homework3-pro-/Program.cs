cfc0b2c81d8bca0904fe08d0409a72c4ed9f29e0
scrol <scrol@DESKTOP-CDVGASJ>
2024-01-03 11:21:05 +0200

    Add project files.

---
+++ Homework3-pro-/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+interface IOutput
+{
+    void Show();
+    void Show(string info);
+}
+class MyArray : IOutput
+{
+    private int[] array;
+
+    public MyArray(int[] arr)
+    {
+        array = arr;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Елементы масива");
+        foreach (var element in array)
+        {
+            Console.WriteLine(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine("Элементы масива:");
+        foreach(var element in array)
+        {
+            Console.WriteLine(element + " ");
+        }
+        Console.WriteLine($"\nИнформация: {info}");
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] myIntArray = { 1, 2, 3, 4, 5 };
+        MyArray myArrayObj = new MyArray(myIntArray);
+
+        myArrayObj.Show();
+        myArrayObj.Show("Тестовое сообщение");
+
+    }
+}
